140775eb6a32237a6564ab1fac938a46c08c13e0
Yurii Cherkasov <strategarius@protonmail.com>
2023-01-20 10:58:54 +0200

    Update submodules function fix

--- cmake/update-submodules.cmake
+++ cmake/update-submodules.cmake
@@ -1,9 +1,15 @@
 function(update_submodules submodules_list)
     if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.gitmodules")
         if ("${submodules_list}" STREQUAL "")
-            execute_process(COMMAND git submodule update --init --remote --recursive
+            execute_process(COMMAND git submodule update --init --recursive
                             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
         else()
             execute_process(COMMAND git submodule init ${submodules_list}
                             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-            execute_process(COMMAND git submodule update --recursive --remote ${submodules_list}
+            execute_process(COMMAND git submodule update --recursive ${submodules_list}
+                            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        endif()
+    else()
+        message(WARNING ".gitmodules file not found in ${CMAKE_CURRENT_SOURCE_DIR}.")
+    endif()
+endfunction()
