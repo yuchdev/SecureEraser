54e2f54241e485c54cfae9f9de63c905f096ab89
Yurii Cherkasov <strategarius@protonmail.com>
2023-01-21 11:01:16 +0200

    Last edits readme.md

--- appcli/sources.cmake
+++ appcli/sources.cmake
@@ -1,3 +1,8 @@
 set(APPCLI_SOURCES
-    src/main.cpp
+        src/main.cpp
+        src/command_line_parser.cpp
+        include/appcli/command_line_parser.h
 )
+set(APPCLI_INCLUDES
+        include/appcli/command_line_parser.h
+        )
