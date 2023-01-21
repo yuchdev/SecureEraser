7798959b8fc4882a44ea7746cc288590023123ff
Yurii Cherkasov <strategarius@protonmail.com>
2023-01-21 11:09:03 +0200

    Comamnd-line interafce

--- appcli/include/appcli/command_line_parser.h
+++ appcli/include/appcli/command_line_parser.h
@@ -1,1 +1,67 @@
+/* 2017-2020 WEBGEARS SERVICES LIMITED (c) All Rights Reserved. 
+ * Proprietary and confidential.
+ * The Software and any accompanying documentation are copyrighted and protected 
+ * by copyright laws and international copyright treaties, as well as other 
+ * intellectual property laws and treaties.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ */
+
 #pragma once
+#include <memory>
+#include <string>
+#include <boost/program_options.hpp>
+
+// The header contains command-line parser for the PanicButton service application
+// It uses Boost.ProgramOptions
+
+namespace panic {
+
+/**@brief Storage for all passed and default command-line params */
+class CommandLineParams {
+public:
+
+    /// @brief Should have a constructor by default
+    CommandLineParams();
+
+    /// @brief Parse raw command-line parameters
+    void read_params(int argc, char* argv[]);
+
+    /// @brief Set some logical param
+    void set_flag(const boost::program_options::variables_map& vm, bool& flag, const char* str);
+
+    /// @brief Get available options list
+    boost::program_options::options_description& options_descript(){ return cmd_options_description; }
+
+    //////////////////////////////////////////////////////////////////////////
+    /// Accessors
+
+    /// @brief Print command line help
+    bool is_help() const 
+    {
+        return _help;
+    }
+
+    /// @brief Print service version (from CMake-generated version.h file)
+    bool is_version() const 
+    {
+        return _version;
+    }
+
+
+
+private:
+
+    /// Show help
+    bool _help = false;
+
+    /// Show version
+    bool _version = false;
+
+    /// Log filename if erase
+    std::string _erase_filename;
+
+    ///
+    boost::program_options::options_description cmd_options_description;
+};
+
+} // namespace panic
--- appcli/src/command_line_parser.cpp
+++ appcli/src/command_line_parser.cpp
@@ -1,1 +1,50 @@
-#include <appcli/command_line_parser.h>
+/* 2017-2020 WEBGEARS SERVICES LIMITED (c) All Rights Reserved. 
+ * Proprietary and confidential.
+ * The Software and any accompanying documentation are copyrighted and protected 
+ * by copyright laws and international copyright treaties, as well as other 
+ * intellectual property laws and treaties.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ */
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <algorithm>
+#include <boost/filesystem.hpp>
+
+#include <panic_service/command_line_parser.h>
+
+using namespace panic;
+
+namespace po = boost::program_options;
+namespace bf = boost::filesystem;
+
+using std::string;
+
+CommandLineParams::CommandLineParams() : cmd_options_description("Command-line interface has the following options")
+{}
+
+void CommandLineParams::read_params(int argc, char* argv[])
+{
+    cmd_options_description.add_options()
+        ("help,h", "Print usage")
+        ("version,V", "Print version")
+        ("erase,e", po::value<string>(&_erase_filename), "Erase file")
+        ;
+
+    // command line params processing
+    po::variables_map cmd_variables_map;
+    po::store(parse_command_line(argc, argv, cmd_options_description), cmd_variables_map);
+    po::notify(cmd_variables_map);
+
+    set_flag(cmd_variables_map, _help, "help");
+    set_flag(cmd_variables_map, _version, "version");
+}
+
+/**@brief Set some logical param */
+void CommandLineParams::set_flag(const po::variables_map& vm, bool& flag, const char* str)
+{
+    if (vm.count(str)) {
+        flag = true;
+    }
+}
